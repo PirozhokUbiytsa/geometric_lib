c224e2c159b38ffa3e3cc183df062cbd8167402b
PirozhokUbiytsa <113065869+PirozhokUbiytsa@users.noreply.github.com>
2024-02-04 23:31:22 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,10 +2,7 @@ import math
 
 def area(r):
     '''takes a number r, returns the area of a circle of radius r'''
-    if r <= 0:
-        raise ValueError('radius cannot be zero or negative')
-    else:
-        return math.pi * r * r
+    return math.pi * r * r
 
 def perimeter(r):
     '''takes a number r, returns the perimeter of a circle of radius r'''
